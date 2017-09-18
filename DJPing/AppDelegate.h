c8d447ecc8ce277499e0f2a1128df895e9e7ae04
qiushuitian <qiushuitian1111@126.com>
2017-09-18 11:56:52 +0800

    new

---
+++ DJPing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJPing
+//
+//  Created by jian deng on 08/09/2017.
+//  Copyright © 2017 jian deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
